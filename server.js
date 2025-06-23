8b7a1f8373affc820a2fd7c3db9f5f8d1c05bd68
SystemTaipanOne <system.taipan.one@gmail.com>
2025-06-22 21:23:30 -0300

    correçao de fetch e cors

--- server.js
+++ server.js
@@ -11,6 +11,13 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // ou use um domínio específico em produção
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
+
 // INSERIDO: Configura o Express para servir arquivos estáticos da pasta 'public'
 app.use(express.static(path.join(__dirname, 'public')));
 
